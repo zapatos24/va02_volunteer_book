5ff5315b33aa915cb7fad2b4ce3d98b26b00f47f
Jeremy Owens <43456262+zapatos24@users.noreply.github.com>
2020-09-03 19:05:40 -0400

    join smaller sql queries to fully modded table of event participants

---
+++ va02_event_participant_moded.sql
@@ -0,0 +1,51 @@
+-- cleaned event participants with today-14 and today-30
+select p.vanid, p.date, p.name, p.phone, p.status, p.recruited_by, p.signup_date,
+			 p.today14, p.today30, r.organizer, lr.last_recruit
+from(
+	 select *, today-14 as today14, today-30 as today30
+		from(
+  			select vanid, date, name, phone, status, recruited_by, 
+      			   signup_date, getdate() as today
+  			from public.va02_event_participants
+        )
+    ) as p
+
+--
+left outer join
+-- add turfed FO
+
+(
+  select vanid, organizer from public.region_assignment
+) as r
+on p.vanid = r.vanid
+
+--
+left outer join
+--add latest recruit
+
+(
+  select distinct vanid,
+         last_value(recruited_by) over (
+              partition by vanid
+              order by date asc, time asc
+              rows between unbounded preceding and unbounded following) as last_recruit
+  from public.va02_event_participants
+  where recruited_by is not null
+  order by vanid
+) as lr
+on p.vanid = lr.vanid
+
+--
+left outer join
+-- add scheduled boolean
+
+(
+  select distinct vanid, 
+  			 last_value(date) over (
+           		partition by vanid
+           		order by date asc, time asc
+           		rows between unbounded preceding and unbounded following) as last_shift
+) as ls
+on p.vanid = ls.vanid
+
+  
