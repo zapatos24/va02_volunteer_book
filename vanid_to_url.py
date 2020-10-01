0f04c161cfe6ed0a4f505ef5d2329cc41acd40a7
Jeremy Owens <43456262+zapatos24@users.noreply.github.com>
2020-10-01 11:48:39 -0400

    add commenting to better explain functions

--- vanid_to_url.py
+++ vanid_to_url.py
@@ -7,6 +7,7 @@ import civis
 client = civis.APIClient()
 me = client.users.list_me()
 
+# take VAN ID and transform to reversed hex number plus mod 17 checksum as letter
 def van_string(vanid):
     vanid = int(vanid)
     q_entry = 'ABCDEFGHIJKLMNOPQ'
@@ -15,6 +16,7 @@ def van_string(vanid):
     return start + end
 
 
+# append the url string to the beginning of the transformed VAN ID
 def id_to_url(vanid):
     return 'https://www.votebuilder.com/ContactsDetails.aspx?Vanid=EID' + van_string(vanid)
     
@@ -25,9 +27,11 @@ def main():
                              use_pandas=True)
     print(df.head())
     
+    # make the url column from the VAN ID
     df['myc_url'] = df['vanid'].apply(lambda vanid: id_to_url(vanid))
     df.sort_values('date', inplace=True)
     
+    # write the new dataframe to the url table in civis
     civis.io.dataframe_to_civis(df, database='DigiDems', table='sandbox_va_2.va02_event_p_url',
                                 existing_table_rows='drop')
 
